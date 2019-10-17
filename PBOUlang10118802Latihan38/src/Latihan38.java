42e1eb21e4ecb7a85da88de9870e45a8dacc3287
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-10-17 22:19:26 +0700

    Add files via upload

---
+++ PBOUlang10118802Latihan38/src/Latihan38.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Latihan38 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Lingkaran lingkaran = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        lingkaran.validasiInput();
+        lingkaran.hasilPerhitungan(lingkaran.diameter);
+        
+    }
+    
+}
